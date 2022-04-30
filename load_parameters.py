eee6bb7fd16fb92d8f645cc7fe585fc1405a5bcc
AhmedMohamedHisham <1700190@eng.asu.edu.eg>
2022-05-01 00:49:01 +0200

    updating the path

--- load_parameters.py
+++ load_parameters.py
@@ -2,7 +2,7 @@ import pickle
 from os.path import join
 
 # Where did you save the camera calibration results? pickle files
-calibration_outputs_dir = 'output/camera_cal'
+calibration_outputs_dir = 'output/camera calibration'
 
 # Filename used to save the camera calibration result (mtx,dist)
 calibration_mtx_dist_filename = 'camera_cal_dist_pickle.p'
@@ -12,10 +12,10 @@ calibration_m_minv_filename = 'perspective_trans_matrices.p'
 
 
 def load_camera_mtx_dist_from_pickle():
-    '''
+    """
     Read in the saved camera matrix and distortion coefficients
     These are the arrays we calculated using cv2.calibrateCamera()
-    '''
+    """
 
     dist_pickle = pickle.load(open(join(calibration_outputs_dir, calibration_mtx_dist_filename), "rb"))
     mtx = dist_pickle["mtx"]
